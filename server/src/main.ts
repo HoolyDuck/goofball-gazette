9d8b0e9bce27701b326d336ed1433ab9134491ef
Nazariy <nazar260804@gmail.com>
2023-03-26 23:31:37 +0300

    adjusted cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    rawBody : true,
+  });
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+    preflightContinue: false,
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
