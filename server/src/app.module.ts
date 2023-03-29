cf8bbedeb66067d2f96438d3fd3305c071f9855f
Nazariy <nazar260804@gmail.com>
2023-03-29 22:52:07 +0300

    moved user.entity to another folder

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
+import { User } from './entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
--- server/src/auth/local.strategy.ts
+++ server/src/auth/local.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserCreateDto } from 'src/user/dto/user-create.dto';
-import { User } from 'src/user/user.entity';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
