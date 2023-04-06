31e23e600c3d6ac3b8601a4c40747fe7a640b300
Nazariy <nazar260804@gmail.com>
2023-04-07 00:55:13 +0300

    created temporary design for blogpost previews

--- client/src/components/blogpost/BlogPost.tsx
+++ client/src/components/blogpost/BlogPost.tsx
@@ -9,9 +9,10 @@ type BlogPostProps = {
 export function BlogPost(props: BlogPostProps) {
     return (
         <div className="blogpost">
+            <img src="https://i.pinimg.com/736x/8f/dc/3b/8fdc3b8300604ed2af0e86142dc47443.jpg"></img>
             <p className="blogpost__title">{props.title}</p>
             <span className='line'> </span>
-            <p className="blogpost__content">{props.content}</p>
+            <p className="blogpost__content">Short description</p>
         </div>
     )
 }
