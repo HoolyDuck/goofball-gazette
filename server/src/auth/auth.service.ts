c7290f779a6322cac84dfb5f532e5222184aae63
Nazariy <nazar260804@gmail.com>
2023-06-08 21:25:45 +0300

    types, dtos, cleanup and refresh method added

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,16 @@
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { UserCreateDto } from 'src/user/dto/user-create.dto';
 import * as bcrypt from 'bcryptjs';
 import { User } from 'src/entities/user.entity';
+import { TokenDto } from 'src/dto/token.dto';
+import { RegistrationDto } from 'src/dto/registration.dto';
+import { UserInfoDto } from 'src/dto/user-info.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,11 +19,13 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async login(user: User) {
+  async login(user: User): Promise<TokenDto> {
     return this.generateJWT(user);
   }
 
-  async register(user: UserCreateDto): Promise<{ newUser: any, token: string }> {
+  async register(
+    user: RegistrationDto,
+  ): Promise<{ userInfo: UserInfoDto; token: string }> {
     const userExists = await this.userService.findByEmail(user.email);
 
     if (userExists) {
@@ -31,16 +40,26 @@ export class AuthService {
 
     const { access_token } = await this.generateJWT(newUser);
 
-    return { 
-      newUser: { 
+    return {
+      userInfo: {
         id: newUser.id,
         username: newUser.username,
-        email: newUser.email,
-      }, token: access_token };
-
+      },
+      token: access_token,
+    };
   }
 
-  private async generateJWT(user: User) {
+  public async refresh(user: User): Promise<{userInfo: UserInfoDto, token: string}> {
+    return {
+      userInfo: {
+        id: user.id,
+        username: user.username,
+      },
+      token: await this.generateJWT(user).then((res) => res.access_token),
+    }
+  }
+
+  private async generateJWT(user: User): Promise<TokenDto> {
     const payload = {
       id: user.id,
       username: user.username,
@@ -50,10 +69,12 @@ export class AuthService {
     };
   }
 
-  public async validateUser(credential: string, password: string) : Promise<User> {
+  public async validateUser(
+    credential: string,
+    password: string,
+  ): Promise<User> {
     const findUser = await this.userService.findByEmail(credential);
     if (findUser && (await bcrypt.compare(password, findUser.password))) {
-
       return findUser;
     }
     throw new UnauthorizedException('Invalid credentials');
