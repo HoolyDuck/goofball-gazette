637c71c97cc57bb3b45f62dccf1722ea7a2dbd3e
Nazariy <nazar260804@gmail.com>
2023-07-04 16:57:10 +0300

    moved header block styles to its module

--- client/src/components/pages/login-page/components/header-block/HeaderBlock.tsx
+++ client/src/components/pages/login-page/components/header-block/HeaderBlock.tsx
@@ -1,10 +1,12 @@
+import styles from './HeaderBlock.module.scss';
+
 interface HeaderBlockProps {
   title: string;
   className?: string;
 }
 
 export default function HeaderBlock(props: HeaderBlockProps) {
-  return <h1 className={props.className}>
+  return <h1 className={styles["header-block"] + " " + props.className}>
     {props.title}
   </h1>;
 }
